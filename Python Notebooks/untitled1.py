23bbc7fd8feeb98fb69fa7757087bc7b21d6e80f
jgoni90 <jgoni90@gmail.com>
2017-12-07 08:26:10 +0000

    Add files via upload

---
+++ Python Notebooks/untitled1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  5 13:40:20 2016
+
+@author: goniuj
+"""
+
+import pandas as pd
+
+df = pd.read_html('http://www.espn.com/nhl/statistics/player/_/stat/points/sort/points/year/2015/seasontype/2')
